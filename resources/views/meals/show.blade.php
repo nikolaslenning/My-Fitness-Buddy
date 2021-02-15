eae34c08ec1311cd5f4851ed49bd891b1e2b574d
Nikolas Lenning <nikolaslenning@gmail.com>
2021-02-15 14:17:12 -0700

    added display foods in meals for each meal

--- resources/views/meals/show.blade.php
+++ resources/views/meals/show.blade.php
@@ -8,7 +8,7 @@ Edit Meal
 <h4>Add Foods to {{ $meal->name }}</h4>
 <hr>
 <form action='/meals/{{ $meal->id }}/foods' method='POST'>
-{{ csrf_field() }}
+    {{ csrf_field() }}
     <input type="hidden" name="meal_id" value="{{ $meal->id }}" />
 
     <fieldset class="field-group">
@@ -32,8 +32,33 @@ Edit Meal
     </fieldset>
 
     <button type="submit" class="btn btn-primary">Submit</button>
-
-
 </form>
+
+@if ($meal->foods->isEmpty())
+<h3>{{ $meal->name }} has NO food</h3>
+@else
+<ul class="form-group">
+    @foreach ($meal->foods as $food)
+    <li class="list-group-item">
+        <span><b>Name: {{ $food->name }} <br> FOOD-ID: {{ $food->meal_id}} <br>meal_id: {{ $meal->id }}</b></span>
+        <pre>Carbs: {{ $food->carbohydrates }}</pre>
+        <pre>Protein: {{ $food->protein }}</pre>
+        <pre>Fat: {{ $food->fat }}</pre>
+        <form action="/meals/{{ $meal->id}}/foods/{{ $food->id }}" method="post">
+            {{ csrf_field() }}
+
+            <!-- For mockup; not yet functional. 
+                 If you're interested in getting this to work, 
+                 look into Laravel's Events. -->
+            {{ method_field('DELETE') }}
+
+            <button type="submit" class="btn btn-danger" formmethod="post">
+                Delete
+            </button>
+        </form>
+    </li>
+    @endforeach
+</ul>
+@endif
 
 @stop
