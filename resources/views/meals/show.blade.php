aeff6beeb4b1f963cf179e1286c47ae38ea6bd16
Nikolas Lenning <nikolaslenning@gmail.com>
2021-02-22 11:40:33 -0700

    added html5 validation to foods form

--- resources/views/meals/show.blade.php
+++ resources/views/meals/show.blade.php
@@ -24,22 +24,22 @@ Edit Meal
 
     <fieldset class="field-group">
         <label for="name">Name</label>
-        <textarea class="form-control" name="name" rows="1"></textarea>
+        <textarea type='text' class="form-control" name="name" rows="1" required></textarea>
     </fieldset>
 
     <fieldset class="field-group">
         <label for="carbohydrates">Carbohydrates</label>
-        <textarea class="form-control" name="carbohydrates" rows="1"></textarea>
+        <textarea type='number' class="form-control" name="carbohydrates" rows="1" required></textarea>
     </fieldset>
 
     <fieldset class="field-group">
         <label for="protein">Protein</label>
-        <textarea class="form-control" name="protein" rows="1"></textarea>
+        <textarea type='number' class="form-control" name="protein" rows="1" required></textarea>
     </fieldset>
 
     <fieldset class="field-group">
         <label for="fat">Fat</label>
-        <textarea class="form-control" name="fat" rows="1"></textarea>
+        <textarea type='number' class="form-control" name="fat" rows="1" required></textarea>
     </fieldset>
 
     <button type="submit" class="btn btn-primary">Submit</button>
